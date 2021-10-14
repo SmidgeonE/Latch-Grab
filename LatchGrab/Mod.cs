a925bd33b8299ebbf2d46e845efc910347b6d8e3
SmidgeonE <finlaynugent@gmail.com>
2021-10-14 16:14:46 +0100

    Added pistol logic + fixed streamlined bug

--- LatchGrab/Mod.cs
+++ LatchGrab/Mod.cs
@@ -3,6 +3,7 @@ using System.Runtime.Remoting.Services;
 using Deli.Setup;
 using FistVR;
 using HarmonyLib;
+using UnityEngine;
 using Valve.VR;
 
 namespace LatchGrab
@@ -18,7 +19,7 @@ namespace LatchGrab
         
         [HarmonyPatch(typeof(ClosedBolt), "UpdateInteraction")]
         [HarmonyPostfix]
-        private static void InteactionPatch(ClosedBolt __instance)
+        private static void ClosedBoltPatch(ClosedBolt __instance)
         {
             var input = __instance.m_hand.Input;
             bool isPressingDown;
@@ -32,18 +33,43 @@ namespace LatchGrab
                 __instance.LockBolt();
         }
         
+        [HarmonyPatch(typeof(HandgunSlide), "UpdateInteraction")]
+        [HarmonyPostfix]
+        private static void PistolPatch(HandgunSlide __instance)
+        {
+            var handgun = __instance.Handgun;
+            
+            if (!handgun.HasSlideRelease)
+            {
+                Debug.Log("This handgun does not have a slide");
+                return;
+            }
+            
+            var input = __instance.m_hand.Input;
+            bool isPressingDown;
+            
+            if (_controlMode == ControlOptions.CoreControlMode.Standard)
+                isPressingDown = input.TouchpadSouthPressed && input.TouchpadPressed;
+            else
+                isPressingDown = input.TriggerPressed;
+
+            if (isPressingDown && !handgun.IsSlideCatchEngaged() && __instance.LastPos == HandgunSlide.SlidePos.Rear)
+                handgun.EngageSlideRelease();
+        }
+        
         
         
         /* These patches get the current control mode */
         [HarmonyPatch(typeof(GameOptions), "InitializeFromSaveFile")]
-        [HarmonyPrefix]
+        [HarmonyPostfix]
         private static void InitialOptionsGrabPatch(GameOptions __instance)
         {
             _controlMode = __instance.ControlOptions.CCM;
+            Debug.Log("mode set to : " + _controlMode);
         }
         
         [HarmonyPatch(typeof(GameOptions), "SaveToFile")]
-        [HarmonyPrefix]
+        [HarmonyPostfix]
         private static void UpdateControlOptionsPatch(GameOptions __instance)
         {
             _controlMode = __instance.ControlOptions.CCM;
